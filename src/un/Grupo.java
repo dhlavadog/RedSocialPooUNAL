5006f520941e1a12ba0e4157ea41d2592f0de8fb
PC <PC@Diego>
2016-09-17 13:53:43 -0500

    Agregada clase "grupo"

---
+++ src/un/Grupo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package un;
+
+/**
+ *
+ * @author PC
+ */
+public class Grupo 
+{
+    private String grupo;
+    private String descripcion;
+    private String tipoGrupo;
+    private String noticias;
+    private Usuario propietario;
+    
+    Grupo(String g, String tipo, String n, Usuario p)
+    {
+        
+    }
+    
+    
+}
